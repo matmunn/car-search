8c93ebb0c2d27a628a81bf004756cb18fda3d782
Mat Munn <mat@matmunn.me>
2020-10-30 12:57:23 +1100

    Add some settings

--- car_search/settings.py
+++ car_search/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^t@+vy=a&xq7#22s@obwby3n&4lcyfsc0fm=c&rrf*!&i#t#--'
+SECRET_KEY = os.environ.get('SECRET_KEY', '^t@+vy=a&xq7#22s@obwby3n&4lcyfsc0fm=c&rrf*!&i#t#--')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
